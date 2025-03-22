adee73bfd1fcd27a3f9a6e4090d94332dde943dd
Sayan Nandi <sayannandi11071998@gmail.com>
2025-03-22 18:57:53 +0530

    Coins Added in the Game Level

--- Assets/Scripts/Interactables/CoinPickup.cs
+++ Assets/Scripts/Interactables/CoinPickup.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinPickupSFX;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
